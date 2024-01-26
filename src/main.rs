19a3a2d1f9d378d7420eb27ce14b566f8ee59952
Eugene Matsumura <eugene.bb1@gmail.com>
2024-01-26 22:13:26 +0100

    feat: add options and ability to pass scripts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use getopts::Options;
+use std::{env, fs, process};
 
 #[derive(Debug, PartialEq)]
 enum Operation {
@@ -12,13 +13,16 @@ enum Operation {
     In,
 }
 
-fn main() {
-    let content = fs::read_to_string("./test.bf").unwrap();
+fn usage(progname: &str, opts: getopts::Options) {
+    let brief = format!("Usage: {progname} [options] [file]");
+    let usage = opts.usage(&brief);
+    eprint!("{usage}");
+}
+
+fn collect_operations(content: String) -> Vec<Operation> {
     let mut ops: Vec<Operation> = vec![];
     let mut jps: Vec<i32> = vec![];
     let mut jp_counter = 0;
-
-    let buffer_size = 30000;
 
     let mut current_char = ' ';
     let mut repetition_count = 1;
@@ -78,6 +82,10 @@ fn main() {
         ops.push(op);
     }
 
+    ops
+}
+
+fn compile_operations(ops: Vec<Operation>, buffer_size: usize) -> String {
     let mut asm = String::new();
 
     asm += "format ELF64 executable\n";
@@ -145,7 +153,80 @@ fn main() {
     asm += "segment readable writable\n";
     asm += &format!("buf: rb {buffer_size}\n");
 
-    fs::write("./test.asm", asm).unwrap();
+    asm
+}
 
-    println!("{ops:?}");
+fn run() -> i32 {
+    let args: Vec<String> = env::args().collect();
+    let progname = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Print help and exit");
+    opts.optopt("o", "output", "Output file name", "filename");
+    opts.optopt("b", "buffersize", "Length of the brainf**k buffer", "30000");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(x) => x,
+        Err(x) => {
+            eprintln!("{x}");
+            usage(&progname, opts);
+            return 1;
+        }
+    };
+
+    if matches.opt_present("h") {
+        usage(&progname, opts);
+        return 0;
+    }
+
+    let output_path = match matches.opt_str("o") {
+        Some(x) => x,
+        None => String::from("a.asm"),
+    };
+
+    let buffer_size: usize = match matches.opt_str("b") {
+        Some(x) => match x.parse() {
+            Ok(x) => x,
+            Err(x) => {
+                eprintln!("{x}");
+                return 1;
+            }
+        },
+        None => 30000,
+    };
+
+    let file_path = match matches.free.get(0) {
+        Some(x) => x,
+        None => {
+            eprintln!("No input file specified!");
+            return 1;
+        }
+    };
+
+    let content = match fs::read_to_string(file_path) {
+        Ok(c) => c,
+        Err(x) => {
+            eprintln!("Could not read file {file_path}:");
+            eprintln!("{x}");
+            return 1;
+        }
+    };
+
+    let ops = collect_operations(content);
+    let asm = compile_operations(ops, buffer_size);
+
+    match fs::write(&output_path, asm) {
+        Err(x) => {
+            eprintln!("Failed to write assembly to {output_path}");
+            eprintln!("{x}");
+            return 1;
+        }
+        _ => {}
+    }
+
+    0
+}
+
+fn main() {
+    process::exit(run());
 }
